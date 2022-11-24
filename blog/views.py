1dc61668f3bc1517701f1fa870da651fb8110063
Jangkeun Lim <ygeat7@gmail.com>
2022-11-24 12:23:30 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+# from django.shortcuts import render
+from .models import Post
+
+from django.views.generic import ListView, DetailView
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/post_list.html'
+
+class PostDetail(DetailView):
+    model = Post
+
+# def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request,
+#        'blog/post_list.html',
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post': post,
+        }
+    )
