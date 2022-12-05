775be21104a24b81fcd39f41a56d8a59e38b8ff1
Jangkeun Lim <ygeat7@gmail.com>
2022-12-06 08:03:35 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from .models import Post, Category
 
 from django.views.generic import ListView, DetailView
@@ -44,5 +44,24 @@ def single_post_page(request, pk):
         'blog/post_detail.html',
         {
             'post': post,
+        }
+    )
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' :post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
         }
     )
