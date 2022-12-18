1683a78483beb1b9d15d159339b1c11b3e0c3cf1
Jangkeun Lim <ygeat7@gmail.com>
2022-12-19 08:02:33 +0900

    구글 로그인 기능 추가하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
 
     "crispy_forms",
     'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     "blog",
     "single_pages"
@@ -127,3 +132,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTHENTICATION_BACKENDS = (
+
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
