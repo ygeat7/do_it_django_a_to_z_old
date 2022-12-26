22f5f9e79b172fd620d37102ba38b4f23131ce59
Jangkeun Lim <ygeat7@gmail.com>
2022-12-27 08:14:01 +0900

    사용자 아바타 보여주기'

--- blog/models.py
+++ blog/models.py
@@ -70,3 +70,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1407/7d8af8cfae55322b/svg/{self.author.email}'
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.utils.text import slugify
+from django.db.models import Q
 
 class PostList(ListView):
     model = Post
@@ -196,3 +197,21 @@ def delete_comment(request, pk):
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
